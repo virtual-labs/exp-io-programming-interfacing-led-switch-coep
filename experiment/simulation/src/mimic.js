4430eec53e7936ade759ef8ba9f0805df2f5ece3
surabhi <ssm.vlab@coeptech.ac.in>
2025-09-03 18:10:47 +0530

    update

--- experiment/simulation/src/mimic.js
+++ experiment/simulation/src/mimic.js
@@ -100,28 +100,11 @@ if (ledTypeVAl == 2){
 
 var r=paper.image("images/resistorSymbol.png",x+400,y+245,120, 55); 
 //var connection_RtoG=paper.path("M"+(x+436)+" "+(y-17.5)+"l 30 0 l 0 30   ").attr({'stroke-width':2});
-var ground= paper.image("images/ground.png",x+480,y+330,50, 21);
 
-var Rcircle=paper.circle(x+363, y-17.5, 0).attr({'fill':'#FF0800'});
-var Ledcircle2=paper.circle(x+330, y+270, 35);
-var terminalRD0=paper.circle(x+158, y+61, 0).attr({'fill':'#FF0800'});
-var terminalRD1=paper.circle(x+142, y+61, 0).attr({'fill':'#FF0800'});
-var terminalRD2=paper.circle(x+127, y+61, 0).attr({'fill':'#FF0800'});
-var terminalRD3=paper.circle(x+111, y+61, 0).attr({'fill':'#FF0800'});
-var terminalRD4=paper.circle(x+28, y+116, 0).attr({'fill':'#FF0800'});
-var terminalRD5=paper.circle(x+28, y+130, 0).attr({'fill':'#FF0800'});
-var terminalRD6=paper.circle(x+28, y+144, 0).attr({'fill':'#FF0800'});
-var  terminalRD7=paper.circle(x+28, y+156.5, 0).attr({'fill':'#FF0800'});
 
-var terminalRC0 = paper.circle(x+208, y+118, 0).attr({'fill':'#1a0dab'});
-var terminalD13 = paper.circle(x+208, y+232, 0).attr({'fill':'#FF0800'});
-var terminalRC1=paper.circle(x+206, y+61, 0).attr({'fill':'#1a0dab'});
-var terminalRC2=paper.circle(x+190, y+61, 0).attr({'fill':'#1a0dab'});
-var terminalRC3=paper.circle(x+174, y+61, 0).attr({'fill':'#1a0dab'});
-var terminalRC4=paper.circle(x+95, y+61, 0).attr({'fill':'#1a0dab'});
-var terminalRC5=paper.circle(x+80, y+61, 0).attr({'fill':'#1a0dab'});
-var terminalRC6=paper.circle(x+64, y+61, 0).attr({'fill':'#1a0dab'});
-var terminalRC7=paper.circle(x+28, y+102, 0).attr({'fill':'#1a0dab'});
+
+var Ledcircle2=paper.circle(x+328, y+270, 35);
+
 
 
 //var ground_circle=paper.circle(x+325,y+263,5).attr({'fill':' #FF0000'});
@@ -131,9 +114,10 @@ var terminalRC7=paper.circle(x+28, y+102, 0).attr({'fill':'#1a0dab'});
 
 var switchCircleNAme=paper.text(x+320, y+85,"Switch").attr({"font-weight":"bold",'font-size': 14});
 var switchCircleNAme=paper.text(x+320, y+102,"Terminal").attr({"font-weight":"bold",'font-size': 14});
+var switchCircleNAme=paper.text(x+250, y+100,"(+5V)").attr({"font-weight":"bold",'font-size': 10});
 var switchCircle=paper.circle(x+308, y+118, 5).attr({'fill':'#1a0dab'});
 var switchbaseImg=paper.image("images/switchPIC2.png",x+358,y+72,101, 50);
-var ardinoPin = paper.image("images/ardinoPin.png",x+50,y+72,151, 200);
+var ardinoPin = paper.image("images/ArdiunoPin.png",x+30,y+72,200, 250);
 var switchTopImg ;
  
 
@@ -144,7 +128,7 @@ if(switchConfigSelection==1){
 	
 }else{
 	console.log("hwt");
-	switchTopImg = paper.image("images/PICswitchTopPreview.png",x+370,y+60,78, 41);
+	switchTopImg = paper.image("images/PICswitchTopPreview.png",x+370,y+50,78, 41);
 //	var resister2=paper.image("images/resistorSymbol.png",x+410,y+137,120, 55).rotate(90); 
 ////	var resister2reading=paper.text(x+490,y+165,"1K").attr({'font-size':14,'stroke':'#800000'});
 //	var sConnection=paper.path("M"+(x+310)+" "+(y+118)+"l 50 -0.7 ").attr({'stroke-width':3});
@@ -155,17 +139,29 @@ if(switchConfigSelection==1){
 var t1 ;
 var ground1;
 
-var t = paper.path("M"+(x+505)+" "+(y+272)+"l  0 60 ").attr({'stroke-width':3});
 
+var ground1= paper.image("images/ground.png",x+210,y+330,30, 21);
+var t1 = paper.path("M"+(x+229)+" "+(y+332)+"l 0 -40  l -10 0 ").attr({'stroke-width':3});
 // LED Connection Sourcning sinking
 if (ledTypeVAl == 1){
-	var t1 = paper.path("M"+(x+70)+" "+(y+300)+"l -60 0  l 0 30").attr({'stroke-width':3});
-	var ground1= paper.image("images/ground.png",x-20,y+330,50, 21);
+//	var t1 = paper.path("M"+(x+70)+" "+(y+300)+"l -60 0  l 0 30").attr({'stroke-width':3});
+	var t = paper.path("M"+(x+505)+" "+(y+272)+"l  0 60 ").attr({'stroke-width':3});
+	var ground= paper.image("images/ground.png",x+480,y+330,50, 21);
+
 	var groundConnection=paper.path("M"+(x+310)+" "+(y+118)+" l 50 -1"
 						+"M"+(x+456)+" "+(y+118)+" l 56 0 ").attr({'stroke-width':3});
 	var resister2=paper.image("images/resistorSymbol.png",x+460,y+52,100, 55).rotate(90);					
 	var vdd2 = paper.image("images/VDD5V.png",x+490,y,55, 51);
 	
+}else{
+//	var t1 = paper.path("M"+(x+70)+" "+(y+300)+"l -60 0  l 0 30").attr({'stroke-width':3});
+	
+	var groundConnection=paper.path("M"+(x+310)+" "+(y+118)+" l 50 -1"
+						+"M"+(x+456)+" "+(y+118)+" l 0 0 ").attr({'stroke-width':3});
+	var resister2=paper.image("images/resistorSymbol.png",x+440,y+90,100, 55);	
+	var t = paper.path("M"+(x+529)+" "+(y+116)+"l  0 38 ").attr({'stroke-width':3});
+	var ground= paper.image("images/ground.png",x+510,y+150,30, 21);				
+	var vdd2 = paper.image("images/VDD5V.png",x+485,y+222,55, 51);
 }
 
 
@@ -182,181 +178,11 @@ resetimg.click(function(){
 })
 
 
-var crossCheckConnect=false;
-var cathodFlag=0;
-
-	var pinName=0;	
-	if(pinVal==1){
-//		pinName=1;
-		D0click=true;
-	}else if(pinVal==2){
-//		pinName=2;
-		D1click=true;
-	}else if(pinVal==3){
-//		pinName=3;
-		D2click=true;
-	}else if(pinVal==4){
-//		pinName=4;			
-		D3click=true;
-	}else if(pinVal==5){
-//		pinName=5;
-		D4click=true;
-	}else if(pinVal==6){
-//		pinName=6;
-		D5click=true;
-	}else if(pinVal==7){
-//		pinName=7;
-		D6click=true;
-	}else if(pinVal==8){
-//		pinName=8;
-		D7click=true;
-	}else{
-//		alert("Wrong Connection.Please Try Again");
-	}
-
-var anodeClick=false;
-var d0Flag=0;
-var d1Flag=0;
-var d2Flag=0;
-var d3Flag=0;
-var d4Flag=0;
-var d5Flag=0;
-var d6Flag=0;
-var d7Flag=0;
-
-var anode_connection_arr2=[];
-var anodeflg=0;
-
-var checkflag1=0;
-var checkflag2=0;
-var checkflag3=0;
-var checkflag4=0;
-var checkflag5=0;
-var checkflag6=0;
-var checkflag7=0;
-var checkflag8=0;
-
-var vfyRD0Connect=false;
-var vfyRD1Connect=false;
-var vfyRD2Connect=false;
-var vfyRD3Connect=false;
-var vfyRD4Connect=false;
-var vfyRD5Connect=false;
-var vfyRD6Connect=false; 
-var vfyRD7Connect=false;
-
-var d13click = false;
-
-terminalD13.click(function(){
-	if(cathodClick == true){
-					anode_connection_arr2[0]=paper.path("M "+(x+210)+" "+(y+232)+ "l 0 0").attr({'stroke-width':3,'stroke':'#FF0800'});			
-					anode_connection_arr2[0].animate({path :"M"+(x+210)+" "+(y+232)+"l 40 0 "},500, function(){
-					anode_connection_arr2[1]=paper.path("M "+(x+250)+" "+(y+232)+ "l 0 0").attr({'stroke-width':3,'stroke':'#FF0800'});			
-					anode_connection_arr2[1].animate({path :"M"+(x+250)+" "+(y+232)+"l 0 -180 "},500, function(){
-					anode_connection_arr2[2]=paper.path("M "+(x+250)+" "+(y+52)+ "l 0 0").attr({'stroke-width':3,'stroke':'#FF0800'});			
-					anode_connection_arr2[2].animate({path :"M"+(x+250)+" "+(y+52)+"l -85 0 "},500, function(){
-					anode_connection_arr2[2]=paper.path("M "+(x+165)+" "+(y+52)+ "l 0 0").attr({'stroke-width':3,'stroke':'#FF0800'});			
-					anode_connection_arr2[2].animate({path :"M"+(x+165)+" "+(y+52)+"l 0 -68 "},500, function(){
-						d13click = true;
-						
-						})   
-					
-						
-						})
-					
-						})
-						})
-						}
-});
 
 
-cathodClick = false;
-cathode.click(function(){
-	cathodClick = true;
-})
-
-var verifyAconnect=false;
-anode.click(function(){
-	anodeflg=1;
-	anodeClick=true;
-	
-	var anode_connection_arr=[];
-	if (diodSelection==0){
-		TtoAconnectionforRDiod2();
-	}else{
-		TtoAconnection();
-	}
-	
-});
 
 
-//VDD Ground and Resister Connections for RC0 to RC7
 
-var switchCircleFlag=0;
-var switchCircleClick=false;
-var vfySwitchConnect=false;
-var connection1 = 0;
-switchCircle.click(function(){
-	console.log("switcheCircle");
-	switchCircleFlag=1;
-	connection1 = 1;
-	switchCircleClick=true;
-	vfySwitchConnect = true;
-	StoT_connection_arr2[0]=paper.path("M "+(x+200)+" "+(y+118)+ "l 0 0").attr({'stroke-width':3 ,'stroke':'#1a0dab'});			
-	StoT_connection_arr2[0].animate({path :"M"+(x+200)+" "+(y+118)+"l 103 0"},500,)
-	
-	
-});
-
-var c0Flag=0;
-var c1Flag=0;
-var c2Flag=0;
-var c3Flag=0;
-var c4Flag=0;
-var c5Flag=0;
-var c6Flag=0;
-var c7Flag=0;
-
-var inputpinName=0;
-
-var inputCheckFlag1=0;
-var inputCheckFlag2=0;
-var inputCheckFlag3=0;
-var inputCheckFlag4=0;
-var inputCheckFlag5=0;
-var inputCheckFlag6=0;
-var inputCheckFlag7=0;
-var inputCheckFlag8=0;
-
-var vfyRc0Connect=false;
-var vfyRc1Connect=false;
-var vfyRc2Connect=false;
-var vfyRc3Connect=false;
-var vfyRc4Connect=false;
-var vfyRc5Connect=false;
-var vfyRc6Connect=false; 
-var vfyRc7Connect=false;
-
-
-var StoT_connection_arr2=[];
-terminalRC0.click(function(){
-c0Flag=1;
-inputCheckFlag1=1;
-if(vfySwitchConnect==true){
-	toastr.success("Already Connected.");
-}else{	
-		console.log("hi");
-		 if(switchCircleFlag==1){
-				StoT_connection_arr2[0]=paper.path("M "+(x+200)+" "+(y+118)+ "l 0 0").attr({'stroke-width':3 ,'stroke':'#1a0dab'});			
-				StoT_connection_arr2[0].animate({path :"M"+(x+200)+" "+(y+118)+"l 100 0"},500, function(){
-					vfyRc0Connect=true;
-					inputpinName=1;
-//					inputCheckFlag1=1;
-				});	
-
-		}
-}	
-});
 
 
 
@@ -409,7 +235,7 @@ checkStatus.click(function(){
 
 
 
-ardinoPin = paper.image("images/ardinoPin.png",x+50,y+72,160, 250);
+//ardinoPin = paper.image("images/ArdiunoPin.png",x+50,y+72,160, 250);
 
 
 var myInterval;
@@ -420,13 +246,10 @@ switchTopImg.mousedown( function() {
 		offTime1 = 0/1000;
 		switchpress.hide();
 		arrowImg.hide();
-		if(switchConfigSelection==1){
+		
 			setRedflag=true;
 		switchTopImg.animate({transform: ['t',0, 10]}, 100);
-		}else{
-			setRedflag=false;
-			switchTopImg.animate({transform: ['t',0, 0]}, 100);
-		}
+		
 		$("#plot").prop("hidden",false);
 		start(interval_plot1,onTime1,offTime1);
 		//myInterval = setInterval(blink, 100); 
@@ -448,13 +271,10 @@ switchTopImg.mouseup( function() {
 
 	//var LedImg1=paper.image("images/offDiode.png", 285, 95, 85, 85);
 //		var arrowImg11=paper.rect(x+389,y+7,40, 45).attr({'stroke':'#fff','fill':'#fff'});
-	if(switchConfigSelection==1){
+	
 		setRedflag=false;
 		switchTopImg.animate({transform: ['t',0, 0]}, 100);
-		}else{
-			setRedflag=true;
-			switchTopImg.animate({transform: ['t',0, -20]}, 100);
-		}
+		
 	$("#plot").prop("hidden",false);
 	start(interval_plot1,onTime1,offTime1);
 		//reset();		
@@ -470,21 +290,21 @@ var	wrongflg = 0;
   // Draw points
   var points = [
     paper.circle(x+268, y+270, 5).attr({fill: "red", stroke: "black", "stroke-width": 2}), 
-    paper.circle(x+210, y+274, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d13
+    paper.circle(x+210, y+274, 0).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d13
     paper.circle(x+390, y+270, 5).attr({fill: "red", stroke: "black", "stroke-width": 2}),
     paper.circle(x+420, y+270, 5).attr({fill: "red", stroke: "black", "stroke-width": 2}),
 	paper.circle(x+305, y+118, 5).attr({fill: "blue", stroke: "black", "stroke-width": 2}),
-	paper.circle(x+210, y+128, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d2
-	paper.circle(x+210, y+140, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d3
-	paper.circle(x+210, y+154, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d4
-	paper.circle(x+210, y+168, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d5
-	paper.circle(x+210, y+182, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d6
-	paper.circle(x+210, y+195, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d7
-	paper.circle(x+210, y+208, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d8
-	paper.circle(x+210, y+220, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d9
-	paper.circle(x+210, y+232, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d10
-	paper.circle(x+210, y+245, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d11
-	paper.circle(x+210, y+258, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d12
+	paper.circle(x+232, y+133, 0).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d2
+	paper.circle(x+232, y+148, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d3
+	paper.circle(x+232, y+163, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d4
+	paper.circle(x+232, y+177, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d5
+	paper.circle(x+232, y+195, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d6
+	paper.circle(x+232, y+214, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d7
+	paper.circle(x+232, y+232, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}) //d8
+//	paper.circle(x+210, y+220, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d9
+//	paper.circle(x+210, y+232, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d10
+//	paper.circle(x+210, y+245, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d11
+//	paper.circle(x+210, y+258, 4.5).attr({fill: "blue", stroke: "black", "stroke-width": 2}), //d12
   ];
 
   // Assign IDs
@@ -505,43 +325,38 @@ var	wrongflg = 0;
     
    if(selectedOutput == 1){
 	    correctSetA = [
-   		 ["P1", "P13"], // d9 to ouput to p1 switch
+   		 ["P1", "P6"], // d9 to ouput to p1 switch
    		 ["P3", "P4"] //led to register
   		];
    }else if (selectedOutput == 2){
 	   correctSetA = [
-		["P1", "P14"], // d10 to ouput 
+		["P1", "P7"], // d10 to ouput 
 	    ["P3", "P4"] //led to register
   		];
    } else if (selectedOutput == 3){
 	   correctSetA = [
-		["P1", "P15"], // d11 to ouput 
+		["P1", "P8"], // d11 to ouput 
 	    ["P3", "P4"] //led to register
   		];
    } 
    else if (selectedOutput == 4){
 	   correctSetA = [
-		["P1", "P2"], // d13 to ouput 
+		["P1", "P9"], // d13 to ouput 
 	    ["P3", "P4"] //led to register
  		 ]	;
    } else if (selectedOutput == 5){
 	   correctSetA = [
-		["P1", "P7"], // d3 to ouput 
+		["P1", "P10"], // d3 to ouput 
 	    ["P3", "P4"] //led to register
  		 ]	;
    }else if (selectedOutput == 6){
 	   correctSetA = [
-		["P1", "P8"], // d4 to ouput 
+		["P1", "p11"], // d4 to ouput 
 	    ["P3", "P4"] //led to register
  		 ]	;
    }else if (selectedOutput == 7){
 	   correctSetA = [
-		["P1", "P9"], // d5 to ouput 
-	    ["P3", "P4"] //led to register
- 		 ]	;
-   }else if (selectedOutput == 8){
-	   correctSetA = [
-		["P1", "P10"], // d6 to ouput 
+		["P1", "P12"], // d5 to ouput 
 	    ["P3", "P4"] //led to register
  		 ]	;
    }
@@ -574,10 +389,6 @@ var	wrongflg = 0;
 	  }else if(selectedValue == 7){
 		 
 		  correctSetB = [ ["P5", "P12"] // d8 to switch p5
-		  ];
-	  }else if(selectedValue == 8){
-		 
-		  correctSetB = [ ["P5", "P13"] // d9 to switch p5
 		  ];
 	  }
 
